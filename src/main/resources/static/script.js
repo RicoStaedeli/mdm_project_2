57ef3b6d7e7ec5e2c3611bcaed5ea78e62094b84
Rico Staedeli ZHAW <staedric@students.zhaw.ch>
2023-04-25 10:23:21 +0200

    add frontend function for predict endpoint

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -4,7 +4,7 @@ const imageElement = document.getElementById("uploaded_image");
 const resultImageElement = document.getElementById("result_image");
 
 // Listen for the "change" event on the input element
-inputElement.addEventListener("change", handleFiles, false);
+inputElement.addEventListener("change", preditImage, false);
 //resultImageElement.src = "../Result/result.png"
 
 // Define the handleFiles function
@@ -36,6 +36,37 @@ function handleFiles() {
             console.log(data)
         })
         .catch(error => console.log('error', error));
+}
+
+function preditImage(){
+    console.log("Start Object detection")
+    const fileList = this.files;
+    if (!fileList.length) {
+        console.log("No file selected");
+        return;
+    }
+    // Create a new FormData object
+    const formData = new FormData();
+
+    // Append the image file to the FormData object
+    formData.append("image", fileList[0]);
+
+    //Bild anzeigen
+    imageElement.src = URL.createObjectURL(this.files[0])
+    var myHeaders = new Headers();
+
+    fetch('/predict', {
+        method: 'POST',
+        headers: myHeaders,
+        body: formData
+    })
+    .then(response => response.blob())
+    .then(blob => {
+        const imageUrl = URL.createObjectURL(blob);
+        console.log(imageUrl)
+        resultImageElement.src = imageUrl;
+    });
+    
 }
 
 function getGeneratedImage() {
